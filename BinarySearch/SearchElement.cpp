8ed028819803842726dff09cc3d894a947b2b062
Shyamal <sgprajapati24@gmail.com>
2022-02-15 20:52:57 +0530

    search element -- Binary search

---
+++ BinarySearch/SearchElement.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+int search(vector<int> &nums, int target){
+    int low = 0, high = nums.size() - 1;
+    while(low<= high){
+        int mid = (low+high)/2;
+        if(nums[mid] == target)
+        return mid;
+
+        if(nums[low] <= nums[mid]){
+            if(nums[low] <= target && nums[mid] >= target)
+            high = mid -1;
+            else
+            low = mid +1;
+        }
+        else{
+            if(nums[mid] <= target && target <= nums[high])
+            low = mid +1;
+            else
+            high = mid-1;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    vector<int> nums = {4,5,6,7,0,1,2,3};
+    int target = 3;
+    cout<<search(nums, target);
+    return 0;
+}
+
+// Time- O(logn)
+// Space- O(1)
+
+/*
+30 mins of learning
+Working on Prakrut project- working on home page 
+Started going Gym from today - 15/02/22
+*/
